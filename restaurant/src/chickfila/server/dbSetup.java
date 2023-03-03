8329eaa392b558468f94385ab2f37d229cdcac6b
AnDuongAdonk <anqduong@tamu.edu>
2023-03-03 10:03:33 -0600

    remove config

---
+++ restaurant/src/chickfila/server/dbSetup.java
@@ -0,0 +1,7 @@
+package chickfila.server;
+
+public final class dbSetup  {
+  //TODO: Update user and pswd
+  public static final String user = "csce315331_duong";
+  public static final String pswd = "531002559";
+}
