ad1324fbc0080c31cc31f95c14cbd9a760e0878f
AnDuongAdonk <anqduong@tamu.edu>
2023-03-06 23:20:03 -0600

    awegw

--- restaurant/src/chickfila/Controller.java
+++ restaurant/src/chickfila/Controller.java
@@ -1,14 +1,21 @@
 package chickfila;
 
+import java.sql.*;
+
+import chickfila.logic.DB;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class Controller {
 
-    @FXML
-    private Label label;
+    private DB conn;
 
     public void initialize() {
 
     }
+
+    public void setConnection(DB db) {
+        conn = db;
+    }
+
 }
--- restaurant/src/chickfila/Main.java
+++ restaurant/src/chickfila/Main.java
@@ -1,8 +1,10 @@
 package chickfila;
 
-import java.util.HashMap;
+import java.sql.*;
+import java.util.*;
 
 import chickfila.logic.DB;
+import chickfila.logic.dbSetup;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -13,17 +15,33 @@ public class Main extends Application {
 
     private DB conn;
     private HashMap<String, Double> menu;
-    
+
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("./hellofx.fxml"));
+    public void start(Stage primaryStage) throws Exception {
+
+        conn = new DB(dbSetup.user, dbSetup.pswd);
+        menu = new HashMap<String, Double>();
+        loadMenu();
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("./hellofx.fxml"));
+        Parent root = loader.load();
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 800, 800));
         primaryStage.show();
     }
 
+    private void loadMenu() throws SQLException {
+        ResultSet menuFetch = conn.select("SELECT * FROM menu_items;");
+
+        while (menuFetch.next()) {
+            String menuName = menuFetch.getString("menu_item_name");
+            double menuPrice = menuFetch.getDouble("menu_item_price");
+            menu.put(menuName, menuPrice);
+        }
+    }
 
     public static void main(String[] args) {
+
         launch(args);
     }
 }
