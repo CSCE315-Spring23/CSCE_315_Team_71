6a00938f95a55ffee9acdad54ed2dc1500605a3c
jbela <73561726+jbela@users.noreply.github.com>
2023-03-06 19:28:15 -0600

    added cashierfxml and moved the hellofx

---
+++ restaurant/src/chickfila/cashier/ControllerCashier.java
@@ -0,0 +1,13 @@
+package chickfila.cashier;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class ControllerCashier {
+    @FXML
+    private Label label;
+
+    public void initialize() {
+
+    }
+}
