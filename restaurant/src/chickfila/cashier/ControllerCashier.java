460e7555e83f754f32dbfeaedff692301a0e0735
jbela <73561726+jbela@users.noreply.github.com>
2023-03-07 18:07:28 -0600

    added the rest of the button events to controller

--- restaurant/src/chickfila/cashier/ControllerCashier.java
+++ restaurant/src/chickfila/cashier/ControllerCashier.java
@@ -70,75 +70,75 @@ public class ControllerCashier {
     public void handleClick(ActionEvent event) {
         Button b = (Button) event.getSource();
 
-        if (b.equals(n8)) {
-            currentOrder.addItem(new OrderItem("nuggets (8ct)", 6));
-        } else if (b.equals(n12)) {
-            currentOrder.addItem(new OrderItem("nuggets (12ct)", 7));
-        } else if (b.equals(gn8)) {
-            currentOrder.addItem(new OrderItem("nuggets grilled (8ct)", 16));
-        } else if (b.equals(gn12)) {
-            currentOrder.addItem(new OrderItem("nuggets grilled (12ct)", 17));
-        } else if (b.equals(cs)) {
-            currentOrder.addItem(new OrderItem("chicken sandwich", 1));
-        } else if (b.equals(csSp)) {
-            currentOrder.addItem(new OrderItem("chicken sandwich spicy", 3));
-        } else if (b.equals(csD)) {
-            currentOrder.addItem(new OrderItem("chicken sandwich deluxe", 2)); 
-        } else if (b.equals(csSpD)) {
-            currentOrder.addItem(new OrderItem("chicken sandwich spicy deluxe", 4)); 
-        } else if (b.equals(csGr)) {
-            currentOrder.addItem(new OrderItem("chicken sandwich grilled", 18));
-        } else if (b.equals(csGrD)) {
-            currentOrder.addItem(new OrderItem("chicken sandwich grilled club", 34)); 
-        } else if (b.equals(fries)) {
-            currentOrder.addItem(new OrderItem("waffle fries (medium)", 12));
-        } else if (b.equals(sM)) {
-            currentOrder.addItem(new OrderItem("market salad", 19));
-        } else if (b.equals(sC)) {
-            currentOrder.addItem(new OrderItem("cobb salad", 38));
-        } else if (b.equals(sS)) {
-            currentOrder.addItem(new OrderItem("southwest salad", 39)); 
-        } else if (b.equals(friesL)) {
-            currentOrder.addItem(new OrderItem("waffle fries (large)", 52));
-        } else if (b.equals(sideSal)) {
-            currentOrder.addItem(new OrderItem("side salad", 55)); 
-        } else if (b.equals(fruitCup)) {
-            currentOrder.addItem(new OrderItem("fruit cup", 54)); 
-        } else if (b.equals(wrap)) {
-            currentOrder.addItem(new OrderItem("chicken cool wrap", 37)); 
-        } else if (b.equals(chips)) {
-            currentOrder.addItem(new OrderItem("waffle chips", 53)); 
-        } else if (b.equals(cookie)) {
-            currentOrder.addItem(new OrderItem("chocolate chunk cookie", 51)); 
-        } else if (b.equals(iceDream)) {
-            currentOrder.addItem(new OrderItem("icedream cone", 49)); 
-        } else if (b.equals(brownie)) {
-            currentOrder.addItem(new OrderItem("chocolate fudge brownie", 50)); 
-        } else if (b.equals(n12Meal)) {
-            currentOrder.addItem(new OrderItem("nuggets (12ct) meal", 31)); 
-        } else if (b.equals(gn8Meal)) {
-            currentOrder.addItem(new OrderItem("nuggets grilled (8ct) meal", 32)); 
-        } else if (b.equals(gn12Meal)) {
-            currentOrder.addItem(new OrderItem("nuggets grilled (12ct) meal", 33)); 
-        } else if (b.equals(csMeal)) {
-            currentOrder.addItem(new OrderItem("chicken sandwich meal", 25)); 
-        } else if (b.equals(csDMeal)) {
-            currentOrder.addItem(new OrderItem("chicken sandwich deluxe meal", 26)); 
-        } else if (b.equals(csSpMeal)) {
-            currentOrder.addItem(new OrderItem("chicken sandwich spicy meal", 27)); 
-        } else if (b.equals(csSpDMeal)) {
-            currentOrder.addItem(new OrderItem("chicken sandwich spicy deluxe meal", 28)); 
-        } else if (b.equals(csGrMeal)) {
-            currentOrder.addItem(new OrderItem("chicken sandwich grilled meal", 29)); 
-        } else if (b.equals(csGrDMeal)) {
-            currentOrder.addItem(new OrderItem("chicken sandwich grilled club meal", 36)); 
-        } else if (b.equals(n8Meal)) {
-            currentOrder.addItem(new OrderItem("nuggets (8ct) meal", 30)); 
-        } else if (b.equals(wrapMeal)) {
-            currentOrder.addItem(new OrderItem("chicken cool wrap meal", 36)); 
-        } else if (b.equals(softDrinkM)) {
-            currentOrder.addItem(new OrderItem("soft drink (medium)", 9)); 
-        } else if (b.equals(softDrinkL)) {
+        // if (b.equals(n8)) {
+        //     currentOrder.addItem(new OrderItem("nuggets (8ct)", 6));
+        // } else if (b.equals(n12)) {
+        //     currentOrder.addItem(new OrderItem("nuggets (12ct)", 7));
+        // } else if (b.equals(gn8)) {
+        //     currentOrder.addItem(new OrderItem("nuggets grilled (8ct)", 16));
+        // } else if (b.equals(gn12)) {
+        //     currentOrder.addItem(new OrderItem("nuggets grilled (12ct)", 17));
+        // } else if (b.equals(cs)) {
+        //     currentOrder.addItem(new OrderItem("chicken sandwich", 1));
+        // } else if (b.equals(csSp)) {
+        //     currentOrder.addItem(new OrderItem("chicken sandwich spicy", 3));
+        // } else if (b.equals(csD)) {
+        //     currentOrder.addItem(new OrderItem("chicken sandwich deluxe", 2)); 
+        // } else if (b.equals(csSpD)) {
+        //     currentOrder.addItem(new OrderItem("chicken sandwich spicy deluxe", 4)); 
+        // } else if (b.equals(csGr)) {
+        //     currentOrder.addItem(new OrderItem("chicken sandwich grilled", 18));
+        // } else if (b.equals(csGrD)) {
+        //     currentOrder.addItem(new OrderItem("chicken sandwich grilled club", 34)); 
+        // } else if (b.equals(fries)) {
+        //     currentOrder.addItem(new OrderItem("waffle fries (medium)", 12));
+        // } else if (b.equals(sM)) {
+        //     currentOrder.addItem(new OrderItem("market salad", 19));
+        // } else if (b.equals(sC)) {
+        //     currentOrder.addItem(new OrderItem("cobb salad", 38));
+        // } else if (b.equals(sS)) {
+        //     currentOrder.addItem(new OrderItem("southwest salad", 39)); 
+        // } else if (b.equals(friesL)) {
+        //     currentOrder.addItem(new OrderItem("waffle fries (large)", 52));
+        // } else if (b.equals(sideSal)) {
+        //     currentOrder.addItem(new OrderItem("side salad", 55)); 
+        // } else if (b.equals(fruitCup)) {
+        //     currentOrder.addItem(new OrderItem("fruit cup", 54)); 
+        // } else if (b.equals(wrap)) {
+        //     currentOrder.addItem(new OrderItem("chicken cool wrap", 37)); 
+        // } else if (b.equals(chips)) {
+        //     currentOrder.addItem(new OrderItem("waffle chips", 53)); 
+        // } else if (b.equals(cookie)) {
+        //     currentOrder.addItem(new OrderItem("chocolate chunk cookie", 51)); 
+        // } else if (b.equals(iceDream)) {
+        //     currentOrder.addItem(new OrderItem("icedream cone", 49)); 
+        // } else if (b.equals(brownie)) {
+        //     currentOrder.addItem(new OrderItem("chocolate fudge brownie", 50)); 
+        // } else if (b.equals(n12Meal)) {
+        //     currentOrder.addItem(new OrderItem("nuggets (12ct) meal", 31)); 
+        // } else if (b.equals(gn8Meal)) {
+        //     currentOrder.addItem(new OrderItem("nuggets grilled (8ct) meal", 32)); 
+        // } else if (b.equals(gn12Meal)) {
+        //     currentOrder.addItem(new OrderItem("nuggets grilled (12ct) meal", 33)); 
+        // } else if (b.equals(csMeal)) {
+        //     currentOrder.addItem(new OrderItem("chicken sandwich meal", 25)); 
+        // } else if (b.equals(csDMeal)) {
+        //     currentOrder.addItem(new OrderItem("chicken sandwich deluxe meal", 26)); 
+        // } else if (b.equals(csSpMeal)) {
+        //     currentOrder.addItem(new OrderItem("chicken sandwich spicy meal", 27)); 
+        // } else if (b.equals(csSpDMeal)) {
+        //     currentOrder.addItem(new OrderItem("chicken sandwich spicy deluxe meal", 28)); 
+        // } else if (b.equals(csGrMeal)) {
+        //     currentOrder.addItem(new OrderItem("chicken sandwich grilled meal", 29)); 
+        // } else if (b.equals(csGrDMeal)) {
+        //     currentOrder.addItem(new OrderItem("chicken sandwich grilled club meal", 36)); 
+        // } else if (b.equals(n8Meal)) {
+        //     currentOrder.addItem(new OrderItem("nuggets (8ct) meal", 30)); 
+        // } else if (b.equals(wrapMeal)) {
+        //     currentOrder.addItem(new OrderItem("chicken cool wrap meal", 36)); 
+        // } else if (b.equals(softDrinkM)) {
+        //     currentOrder.addItem(new OrderItem("soft drink (medium)", 9)); 
+        if (b.equals(softDrinkL)) {
             currentOrder.addItem(new OrderItem("soft drink (large)", 42)); 
         } else if (b.equals(frosLemonade)) {
             currentOrder.addItem(new OrderItem("frosted lemonade", 47)); 
